002bc07aa477e88e4e5b046b16a30e6dd1a14dd8
alex <aleksandrdenysov38@gmail.com>
2025-09-28 02:04:31 +0300

    Initial commit

--- src/MyApp/App.java
+++ src/MyApp/App.java
@@ -3,5 +3,6 @@ package MyApp;
 public class App {
     public static void main(String[] args) {
         System.out.println("This is shop-online app");
+        System.out.println("Shop-online for customers");
     }
 }
