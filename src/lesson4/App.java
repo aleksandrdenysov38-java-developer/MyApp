aa158e81a2d4a012215f6e312e2d4d505a75e091
alex <aleksandrdenysov38@gmail.com>
2025-10-02 21:58:38 +0300

    Initial commit

--- src/lesson4/App.java
+++ src/lesson4/App.java
@@ -7,8 +7,16 @@ public class App {
                 total sales for %d days is EUR %.2f,
                 sales by day is EUR %.2f.
                 """;
-        String textForClient1 = template.formatted(1, "smartphone", 5, 12153.41, 2430.68);
-        String textForClient2 = template.formatted(2, "laptop", 7, 10486.85, 1498.12);
+        int days1 = 5;
+        float sales1 = 12153.41f;
+        int days2= 7;
+        float sales2 = 10486.85f;
+
+        float salesByDay1 = sales1 / days1;
+        float salesByDay2 = sales2 / days2;
+
+        String textForClient1 = template.formatted(1, "smartphone", days1, sales1, salesByDay1);
+        String textForClient2 = template.formatted(2, "laptop", days2, sales2, salesByDay2);
 
         System.out.println(textForClient1);
         System.out.println(textForClient2);
