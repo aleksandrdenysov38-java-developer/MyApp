d4a118b3d13daa2836dddb4eeb1620729fd0a6c7
alex <aleksandrdenysov38@gmail.com>
2025-10-02 23:21:11 +0300

    Initial commit

--- src/lesson4/App.java
+++ src/lesson4/App.java
@@ -2,11 +2,7 @@ package lesson4;
 
 public class App {
     public static void main(String[] args) {
-        String template = """
-                Product No %d : %s,
-                total sales for %d days is EUR %.2f,
-                sales by day is EUR %.2f.
-                """;
+
         int days1 = 5;
         float sales1 = 12153.41f;
         int days2= 7;
@@ -15,10 +11,12 @@ public class App {
         float salesByDay1 = sales1 / days1;
         float salesByDay2 = sales2 / days2;
 
-        String textForClient1 = template.formatted(1, "smartphone", days1, sales1, salesByDay1);
-        String textForClient2 = template.formatted(2, "laptop", days2, sales2, salesByDay2);
 
-        System.out.printf(textForClient1);
-        System.out.printf(textForClient2);
+
+        System.out.printf("Product No 1: smartphone, \ntotal sales for %d days is EUR %.2f, \n" +
+                "sales by day is EUR %.2f.\n", days1, sales1, salesByDay1);
+        System.out.printf("Product No 2: laptop,  \ntotal sales for %d days is EUR %.2f, "
+                + '\n' + "sales by day is EUR %.2f.", days2, sales2, salesByDay2);
+
     }
 }
