efb1e3db58ca9a5231b0c0926cd337f75a5eb7c8
alex <aleksandrdenysov38@gmail.com>
2025-10-12 13:16:16 +0300

    Initial commit

--- src/lesson7/array.java
+++ src/lesson7/array.java
@@ -14,7 +14,7 @@ public class array {
             }
         System.out.println("Элементы массива: " + Arrays.toString(myArray));
 
-      //  int[] myArray = {34, -10, 56, -22, 78, 5, 7, -15, 42, -30, 91, 0, -50, 18, 99, -3, 25, 11, 63, -8};
+
         int sumOfNegatives = Arrays.stream(myArray).filter(number -> number < 0).sum();
         System.out.println("Сумма отрицательных чисел: " + sumOfNegatives);
 
